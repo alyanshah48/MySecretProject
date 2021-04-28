ded8c5885ecf2c65c16f6b2d359cdc540cbfa0e8
alyanshah48 <83280578+alyanshah48@users.noreply.github.com>
2021-04-28 13:06:41 +0500

    PUrple

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-$colors = array("Red", "Green", "Blue", "Yellow", "Orange");
+$colors = array("Red", "Green", "Blue", "Yellow", "Orange","Blue2", "PUrple");
  
 // Loop through colors array
 foreach($colors as $value){
     echo $value . "<br>";
 }
+
+
 ?>
